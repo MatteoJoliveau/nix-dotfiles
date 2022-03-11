cc3962452badac79266469d6e0a942ae35d090b6
Matteo Joliveau <matteo@matteojoliveau.com>
2022-03-11 15:41:07 +0100

    Enable battery notification

--- nixos/frenchpenguin/configuration.nix
+++ nixos/frenchpenguin/configuration.nix
@@ -53,6 +53,33 @@
   };
   powerManagement.powertop.enable = true;
 
+  # Thanks https://github.com/anderspapitto/nixos-configuration/blob/master/laptop-base.nix
+  systemd.services.battery_check = {
+    description = "Send notification if battery is low";
+    serviceConfig = {
+      Type = "oneshot";
+      User = "anders";
+      ExecStart = pkgs.writeScript "battery_check" ''
+        #!${pkgs.bash}/bin/bash --login
+        . <(udevadm info -q property -p /sys/class/power_supply/BAT0 |
+            grep -E 'POWER_SUPPLY_(CAPACITY|STATUS)=')
+        if [[ $POWER_SUPPLY_STATUS = Discharging && $POWER_SUPPLY_CAPACITY -lt  ]];
+        then notify-send -u critical "Battery is low: $POWER_SUPPLY_CAPACITY";
+        fi
+      '';
+    };
+    environment = { DISPLAY = ":0"; };
+    after = [ "display-manager.service" ];
+    startAt = "*:00/5";
+  };
+  services = {
+    acpid.enable = true;
+
+    logind.extraConfig = ''
+      HoldoffTimeoutSec=0
+    '';
+  };
+
   # Enable Thunderbolt
   services.hardware.bolt.enable = true;
 
